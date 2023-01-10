4b40368cafef5efa51267ab2a0199dbf41bd4195
root <root@ip-172-31-59-59.ec2.internal>
2023-01-10 22:57:23 +0000

    hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,9 @@
+<<<<<<< Updated upstream
 hello git i am iin master branch
+=======
+hello git i am iin master branch 
+ edited in child branch 123 
+again edited in child branch
+i think i am in my master branch 
+
+>>>>>>> Stashed changes
